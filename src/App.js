1c127a5089349c6e0a42a57b711a52cb9a46afe2
armenio <dehme.armenio@gmail.com>
2019-05-08 20:17:15 -0300

    grid

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import GeneralPanel from './View/Panel/GeneralPanel';
-
+import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <GeneralPanel />
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <Header />
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+            <GeneralPanel />
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12">
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
