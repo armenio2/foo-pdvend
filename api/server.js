7b8af85136fb1198d624722635f9af9e48762946
armenio <dehme.armenio@gmail.com>
2019-05-13 14:16:05 -0300

    create node api to consume

---
+++ api/server.js
@@ -0,0 +1,10 @@
+const customers = require('./customers/customers.json')
+const data = customers
+
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.get('/api/v1/customers', (req, res) => res.send(data))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
