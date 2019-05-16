46bc28cba2919d5fc6d56cd7800fd3a558da107e
armenio <dehme.armenio@gmail.com>
2019-05-16 19:11:27 -0300

    foo

--- src/View/Panel/GeneralPanel.jsx
+++ src/View/Panel/GeneralPanel.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './GeneralPanel.css'
 import Card from '../../Util/Card/Card';
-import { get_customers } from '../../service/get_customers';
+import GetCustomers from '../../service/get_customers';
+
+async function get_customers() {
+    const result = await GetCustomers
+    console.log("dentro do get_customers ", result)
+    //return formatResult(result.data)
+    if (result.status === 200) {
+        console.log("sucess ", result)
+        return formatResult(result.data.customers)//result.data
+    } else {
+        return "Error"
+    }
+}
+
+function formatResult(result) {
+    console.log("result ", result)
+    const resultPanel = result.reduce((sum, next) => sum + next.subscription_amount, 0)
+    console.log("depois do reduce ", resultPanel)
+    return resultPanel;
+    /*return resultPanel = {
+        totalCliente: totalCliente
+    }*/
+}
+
 
 function GeneralPanel() {
-    const foo = get_customers
-    console.log("get_customerasdasds ", foo)
+    //const [resultCustomers, setCount] = useState(get_customers())
+    /*state = {
+        resultCustomers: get_customers(),
+    }*/
+
+
+    //console.log("get_customerasdasds ", resultCustomers)
     return (
         <div style={style} class='generalPanel'>
             <div class="container-sm">
@@ -16,16 +44,16 @@ function GeneralPanel() {
                 </div>
                 <div class="row">
                     <div class="col-3" >
-                        <Card title='Total de Clientes' value={0} type={null} />
+                        <Card title='Total de Clientes' value={get_customers()} type={null} />
                     </div>
                     <div class="col-3" >
-                        <Card title='Total de Clientes' value={0} type={null} />
+                        <Card title='Clientes Inadimplentes' value={0} type={null} />
                     </div>
                     <div class="col-3" >
-                        <Card title='Total de Clientes' value={0} type={null} />
+                        <Card title='Clientes Adimplentes' value={0} type={null} />
                     </div>
                     <div class="col-3" >
-                        <Card title='Total de Clientes' value={0} type={null} />
+                        <Card title='Total Arrecadado' value={0} type={null} />
                     </div>
                 </div>
             </div>
@@ -40,9 +68,9 @@ function GeneralPanel() {
     );
 }
 
-
 const style = {
     background: '#F5F5F5',
     height: '100vh'
 }
+
 export default GeneralPanel;
--- src/service/get_customers.js
+++ src/service/get_customers.js
@@ -1,41 +1,16 @@
 const axios = require('axios');
 
-async function get_customers() {
-    let res = await axios.get('http://localhost:5000/api/v1/customers');
-    return res;
-}
-console.log("getcustomers ", get_customers())
-//get_customers();
-/*export const get_customers = axios.get('http://localhost:5000/api/v1/customers')
-    .then(function (response) {
-        // handle success
-        return response
-    })
-    .catch(function (error) {
-        // handle error
-        return console.log(error);
-    })
-    .finally(function () {
-        // always executed
-    });
 
-/*const foo = axios.get('http://localhost:5000/api/v1/customers')
-    .catch(function (error) {
-        if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-        } else if (error.request) {
-            // The request was made but no response was received
-            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-            // http.ClientRequest in node.js
-            console.log(error.request);
-        } else {
-            // Something happened in setting up the request that triggered an Error
-            console.log('Error', error.message);
-        }
-        console.log(error.config);
-    });
-*/
+const get_customers = async () => {
+    return new Promise((result, reject) => {
+        const response = axios.get('http://localhost:5000/api/v1/customers')
+        result(response)
+        reject('erro')
+    })
+    //const response = await axios.get('http://localhost:5000/api/v1/customers')
+    //const data = await response.json()
+    //return response.data
+}
+console.log("fora da function ", get_customers().then())
+export default get_customers();
+//export default getCustomers = get_customers;
