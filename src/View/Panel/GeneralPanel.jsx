70ea422a9ffb408d40aeb8af10711901f319f502
armenio <dehme.armenio@gmail.com>
2019-05-17 11:42:45 -0300

    fix request

--- src/View/Panel/GeneralPanel.jsx
+++ src/View/Panel/GeneralPanel.jsx
@@ -1,72 +1,28 @@
 import React, { useState, useEffect } from 'react';
 import './GeneralPanel.css'
 import Card from '../../Util/Card/Card';
-import GetCustomers from '../../service/get_customers';
+import getCustomers from '../../service/get_customers';
 
 
 
 
+function formatResult(result) {
+    console.log("result ", result)
+    const resultPanel = result.reduce((sum, next) => sum + next.subscription_amount, 0)
+    console.log("depois do reduce ", resultPanel)
+    return resultPanel;
+}
 
-function GeneralPanel() {
+const GeneralPanel = () => {
     const [resultCustomers, setResult] = useState(0)
 
-    /*const get_customers = async () => {
-        const result = await GetCustomers.then()
-        console.log("dentro do get_customers ", result)
-        //return formatResult(result.data)
-        if (result.status === 200) {
-            console.log("sucess ", result)
-            return await formatResult(result.data.customers)//result.data
-        } else {
-            return "Error"
-        }
-    }*/
+    useEffect(async () => {
+        const response = await getCustomers();
+        debugger;
+        setResult(formatResult(response.customers))
+    }, [setResult]);
 
-    function formatResult(result) {
-        console.log("result ", result)
-        const resultPanel = result.reduce((sum, next) => sum + next.subscription_amount, 0)
-        console.log("depois do reduce ", resultPanel)
-        return resultPanel;
-        /*return resultPanel = {
-            totalCliente: totalCliente
-        }*/
-    }
-    /*
-        useEffect(() => {
-            // Update the document title using the browser API
-            console.log("document")
-            setResult(get_customers)
-          });*/
-    useEffect(() => {
-        async function fetchData() {
-            // You can await here
-            const response = await GetCustomers;
-            return response
-            //return formatResult(response.data.customers)
-
-            // ...
-        }
-        fetchData();
-        setResult(fetchData())
-    }, [setResult]); // Or [] if effect doesn't need props or state
-
-
-    function totalCliente(result) {
-        console.log('resultno cliente ', result)
-        return
-    }
-
-    /*useEffect(async () => {
-      async const result = await GetCustomers.then();
-  
-      setResult(formatResult(result.data.customers));
-    });
-*/
     console.log("resultCustomers ", resultCustomers)
-    //let cardResult = resultCustomers.data.customers.reduce((sum, next) => sum + next.subscription_amount, 0)
-    //console.log("cardresult ", cardResult)
-    //setResult(get_customers())
-    //console.log("get_customerasdasds ", resultCustomers)
     return (
         <div style={style} class='generalPanel'>
             <div class="container-sm">
@@ -77,7 +33,7 @@ function GeneralPanel() {
                 </div>
                 <div class="row">
                     <div class="col-3" >
-                        <Card title='Total de Clientes' value={totalCliente(resultCustomers)} type={null} />
+                        <Card title='Total de Clientes' value={resultCustomers} type={null} />
                     </div>
                     <div class="col-3" >
                         <Card title='Clientes Inadimplentes' value={0} type={null} />
--- src/service/get_customers.js
+++ src/service/get_customers.js
@@ -1,16 +1,7 @@
 const axios = require('axios');
 
-
-const get_customers = async () => {
-    return new Promise((result, reject) => {
-        const response = axios.get('http://localhost:5000/api/v1/customers')
-        result(response)
-        reject('erro')
-    })
-    //const response = await axios.get('http://localhost:5000/api/v1/customers')
-    //const data = await response.json()
-    //return response.data
+const getCustomers = async () => {
+    const response = await axios.get('http://localhost:5000/api/v1/customers');
+    return response.data;
 }
-//console.log("fora da function ", get_customers().then())
-export default get_customers();
-//export default getCustomers = get_customers;
+export default getCustomers;
